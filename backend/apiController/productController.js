9387996a773741f9cc5abd13327c5a401d7f5476
huunguyenit <huunguyena6@gmail.com>
2018-05-21 19:05:42 +0700

    sql query with productController

--- backend/apiController/productController.js
+++ backend/apiController/productController.js
@@ -1,22 +1,50 @@
-var db = require('../fn/mysql-db');
+var data = require('../apiController/fn/mysql-db');
 
-exports.loadAll = function() {
-	var sql = 'select * from product';
-	return db.load(sql);
+exports.LoadPage = function(page){
+    var offset = (page-1)*8;
+    var sql = `select * from product order by TimeUp DESC limit ${offset},8`;
+
+    return data.load(sql);
 }
 
-exports.load = function(id) {
-	
+exports.TopTurnPay = function(){
+    var sql = `select * from product ORDER BY TurnPay DESC LIMIT 0,5`;
+
+    return data.load(sql);
 }
 
-exports.add = function() {
-	
+exports.TopPriceNow = function(){
+    var sql = `select * from product order by PriceNow DESC LIMIT 0,5`;
+
+    return data.load(sql);
 }
 
-exports.delete = function(id) {
-	
+exports.TopEndTime = function(){
+    var sql = `SELECT * from product ORDER BY TimeDown LIMIT 0,5`
+
+    return data.load(sql);
 }
 
-exports.update = function() {
-	
+exports.SearchNameProduct = function(name){
+    var sql = `select * from product where ProductName like ${name}`;
+
+    return data.load(sql);
+}
+
+exports.AutoAdd10Minutes = function() {
+    var sql = `UPDATE product SET TimeDown = DATE_ADD(NOW(),INTERVAL 10 MINUTE), AutoUpdate = b'0' WHERE AutoUpdate = b'1' and TimeDown <= DATE_ADD(NOW(),INTERVAL 5 MINUTE)`;
+
+    return data.update(sql);
+} 
+
+exports.AutoAuction = function(Id){
+    var sql = `UPDATE product set PriceNow = PriceNow + Cost WHERE Id = '${Id}'`;
+
+    return data.update(sql);
+}
+
+exports.AddProduct = function(pd){
+    var sql = `insert into product(Id, ProductName, PriceNow, PricePay, TimeUp, TimeDown, Cost, Status, TurnPay, Detail, AutoUpdate) VALUES ('${pd.Id}','${pd.ProductName}' ,'${pd.PriceNow}','${pd.PricePay}','${pd.TimeUp}','${pd.TimeDown}','${pd.Cost}',b'${pd.Status}','${pd.TurnPay}',n'${pd.Detail}', b'${pd.AutoUpdate}') `;
+
+    return data.insert(sql);
 }
