24fff7cf10b810311575d75e6bcf47c2b1071dc5
huunguyenit <huunguyena6@gmail.com>
2018-05-22 11:22:56 +0700

    change some file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', 'views')
-var record = require('./backend/apiController/productController');
+var record = require('./backend/res/productRes.js');
 
 app.listen(5555, () => {
     console.log("server running at port 5555");
