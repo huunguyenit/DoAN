2589a2c94bcad8641a96102c2c40430ede0f6b5c
kietvu2312 <kiet.vu@sysnify.com>
2018-06-05 20:55:40 +0700

    implement 2 apis signin & singup

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,20 @@ var cors = require('cors')
 var product = require('./apiController/ProductController');
 var morgan = require('morgan')
 var http = require('http');
+require('dotenv').config()
+
+
+var AuthRouter = require('./router/auth.router')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(morgan('dev'));
 
+// load auth's router.
+app.use('/auth', AuthRouter)
+
+// load product's router.
 app.use('/product', product)
 
 app.listen(5555, () => {
