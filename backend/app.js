a0ef4c9fd644e9725bab2482366eadb8ee78ad86
kietvu2312 <kiet.vu@sysnify.com>
2018-06-09 17:36:18 +0700

    build skeleton for admin page, implement list user, delete user, stuck at sending email reset password

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ var app = express()
 var bodyParser = require('body-parser')
 var cors = require('cors')
 var morgan = require('morgan')
+var mailer = require('express-mailer')
 var http = require('http');
 require('dotenv').config()
 
 
 var AuthRouter = require('./router/auth.router')
+var AdminRouter = require('./router/admin.router')
 
 var product = require('./apiController/ProductController')
 var user = require('./apiController/UserController')
@@ -17,8 +19,27 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(morgan('dev'))
 
+// add settings for app.
+app.set('view engine', 'ejs')
+
+// setting for mailer.
+mailer.extend(app, {
+    from: process.env.EMAIL_FROM,
+    host: process.env.MAIL_HOST, // hostname
+    secureConnection: process.env.EMAIL_SECURE, // use SSL
+    port: process.env.EMAIL_PORT, // port for secure SMTP
+    transportMethod: process.env.EMAIL_TRANSPORT_METHOD, // default is SMTP. Accepts anything that nodemailer accepts
+    auth: {
+        user: process.env.EMAIL_AUTH_USERNAME,
+        pass: process.env.EMAIL_AUTH_PASSWORD
+    }
+});
+
 // load auth's router.
 app.use('/auth', AuthRouter)
+
+// admin routes.
+app.use('/admin', AdminRouter)
 
 // load product's router.
 app.use('/product', product)
