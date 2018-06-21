684544edf6f4066588cc5d072b70e447576cbb9b
kietvu2312 <kiet.vu@sysnify.com>
2018-06-21 13:11:45 +0700

    element delete request product

--- backend/router/product.router.js
+++ backend/router/product.router.js
@@ -6,6 +6,7 @@ let router = express.Router()
 router.get('', (request, response) => {
     return ProductService.list()
         .then(params => {
+            console.log('params',params);
             return response.render('index', {
                 title: 'Management products',
                 content: params.template,
@@ -16,5 +17,25 @@ router.get('', (request, response) => {
             return response.status(400).send(err)
         })
 })
-
+router.get('/delete/:id/:status', (request, response) => {
+    return ProductService.edit({id:request.params.id, status: request.params.status})
+        .then(params => {
+            console.log('params',params);
+            return   ProductService.list()
+            .then(params => {
+                console.log('params',params);
+                return response.render('index', {
+                    title: 'Management products',
+                    content: params.template,
+                    products: params.products
+                })
+            })
+            .catch(err => {
+                return response.status(400).send(err)
+            })
+        })
+        .catch(err => {
+            return response.status(400).send(err)
+        })
+})
 module.exports = router;
--- backend/service/ProductService.js
+++ backend/service/ProductService.js
@@ -1,13 +1,28 @@
 var mysql = require('../fn/mysql-db')
 
 exports.list = () => {
-    const query = 'SELECT * FROM `product`'
+    const query = 'SELECT * FROM `product` WHERE `Status` = 0'
     const products = mysql.load(query)
     return products
         .then(result => {
             const params = {
                 template: 'product/index.ejs',
                 products: result
+            }
+            return Promise.resolve(params)
+        })
+        .catch(err => {
+            return Promise.reject(err)
+        })
+}
+exports.edit = (params) => {
+    const query = 'UPDATE `product` SET `Status`=' + params.status + ' WHERE `Id` = "' + params.id + '"'
+    const products = mysql.update(query)
+    return products
+        .then(result => {
+            const params = {
+                template: 'product/index.ejs',
+                products: result[0]
             }
             return Promise.resolve(params)
         })
